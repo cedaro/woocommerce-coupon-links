ed0e130e1acfa1ee04192b017c4831a88a0a0d1b
Brady Vercher <brady@blazersix.com>
2016-09-18 12:28:36 -0700

    Use Composer to install WooCommerce and load the testing framework.

--- tests/phpunit/Integration/CouponTest.php
+++ tests/phpunit/Integration/CouponTest.php
@@ -2,19 +2,19 @@
 
 namespace Cedaro\WooCommerce\CouponLinks\Test\Integration;
 
-use Cedaro\WooCommerce\CouponLinks\Test\Framework\Helper\CouponHelper;
-use Cedaro\WooCommerce\CouponLinks\Test\Framework\Helper\ProductHelper;
+use Cedaro\WooCommerce\CouponLinks\Test\Framework\MockSession;
+use WC_Helper_Coupon;
+use WC_Helper_Product;
 
 class CouponTest extends \WP_UnitTestCase {
 	protected $coupon;
-	protected $session_class = '\Cedaro\WooCommerce\CouponLinks\Test\Framework\MockSession';
 
 	public function setUp() {
 		parent::setUp();
 
-		$this->coupon = CouponHelper::create_coupon();
+		$this->coupon = WC_Helper_Coupon::create_coupon();
 
-		WC()->session = new $this->session_class;
+		WC()->session = new MockSession();
 	}
 
 	public function test_add_coupon() {
@@ -26,7 +26,7 @@ class CouponTest extends \WP_UnitTestCase {
 	}
 
 	public function test_add_coupon_after_product() {
-		$product = ProductHelper::create_simple_product();
+		$product = WC_Helper_Product::create_simple_product();
 		$this->go_to( home_url( '/?coupon_code=dummycoupon' ) );
 
 		WC()->cart->add_to_cart( $product->id, 1 );
@@ -36,7 +36,7 @@ class CouponTest extends \WP_UnitTestCase {
 	}
 
 	public function test_add_coupon_before_product() {
-		$product = ProductHelper::create_simple_product();
+		$product = WC_Helper_Product::create_simple_product();
 		$this->go_to( home_url( '/?coupon_code=dummycoupon' ) );
 
 		cedaro_woocommerce_coupon_links();
--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -6,78 +6,94 @@ if ( file_exists( dirname( dirname( __DIR__ ) ) . '/vendor/autoload.php' ) ) {
 	require( dirname( dirname( __DIR__ ) ) . '/vendor/autoload.php' );
 }
 
+class Cedaro_WP_Plugin_Tests_Bootstrap {
+	public function load_tests( $tests_directory ) {
+		$GLOBALS['wp_tests_options'] = [
+			'active_plugins'  => [
+				'woocommerce/woocommerce.php',
+				'woocmmerce-coupon-links/woocommerce-coupon-links.php',
+			],
+			'timezone_string' => 'America/Los_Angeles',
+		];
+
+		// @link https://core.trac.wordpress.org/browser/trunk/tests/phpunit/includes/functions.php
+		require_once $tests_directory . '/includes/functions.php';
+
+		tests_add_filter( 'muplugins_loaded', function() {
+			require( $this->locate_woocommerce() );
+			require( dirname( dirname( __DIR__ ) ) . '/woocommerce-coupon-links.php' );
+		} );
+
+		require $tests_directory . '/includes/bootstrap.php';
+	}
+
+	public function locate_wordpress_tests() {
+		$directories = [
+			getenv( 'WP_TESTS_DIR' ),
+			getenv( 'WP_DEVELOP_DIR' ),
+			'../../../../../tests/phpunit/includes/bootstrap.php',
+			'/tmp/wordpress-tests-lib/includes/bootstrap.php',
+		];
+
+		foreach ( $directories as $directory ) {
+			if ( $directory && file_exists( $directory ) ) {
+				return $directory;
+			}
+		}
+
+		return '';
+	}
+
+	public function locate_woocommerce() {
+		$files = [
+			dirname( dirname( dirname( __DIR__ ) ) ) . '/woocommerce/woocommerce.php',
+			dirname( dirname( __DIR__ ) ) . '/vendor/woocommerce/woocommerce/woocommerce.php',
+			'/tmp/woocommerce/woocommerce.php',
+		];
+
+		foreach ( $files as $file ) {
+			if ( file_exists( $file ) ) {
+				return $file;
+			}
+		}
+
+		return '';
+	}
+
+	public function get_test_suite() {
+		$suite = '';
+
+		$opts = PHPUnit_Util_Getopt::getopt(
+			$GLOBALS['argv'],
+			'd:c:hv',
+			array( 'filter=', 'testsuite=' )
+		);
+
+		foreach ( $opts[0] as $opt ) {
+			if ( '--testsuite' === $opt[0] ) {
+				$suite = $opt[1];
+				break;
+			}
+
+			if ( '--filter' === $opt[0] && false !== stripos( $opt[1], 'unit' ) ) {
+				$suite = 'unit';
+				break;
+			}
+		}
+
+		return strtolower( $suite );
+	}
+}
+
+$bootstrap = new Cedaro_WP_Plugin_Tests_Bootstrap();
+
 /**
  * Load the WordPress tests.
  *
  * Checks to see if a test case in the unit test suite or the unit test suite
  * itself was specified. If not, loads the WordPress tests.
  */
-if ( 'unit' !== _get_test_suite() ) {
-	$_tests_directory = _locate_wordpress_tests_directory();
-	_load_wordpress_tests( $_tests_directory );
-}
-
-function _get_test_suite() {
-	$suite = '';
-
-	$opts = PHPUnit_Util_Getopt::getopt(
-		$GLOBALS['argv'],
-		'd:c:hv',
-		array( 'filter=', 'testsuite=' )
-	);
-
-	foreach ( $opts[0] as $opt ) {
-		if ( '--testsuite' === $opt[0] ) {
-			$suite = $opt[1];
-			break;
-		}
-
-		if ( '--filter' === $opt[0] && false !== stripos( $opt[1], 'unit' ) ) {
-			$suite = 'unit';
-			break;
-		}
-	}
-
-	return strtolower( $suite );
-}
-
-function _load_wordpress_tests( $tests_directory ) {
-	$GLOBALS['wp_tests_options'] = array(
-		'active_plugins'  => array(
-			'woocommerce/woocommerce.php',
-			'woocmmerce-coupon-links/woocommerce-coupon-links.php',
-		),
-		'timezone_string' => 'America/Los_Angeles',
-	);
-
-	require_once $tests_directory . '/includes/functions.php';
-
-	tests_add_filter( 'muplugins_loaded', function() {
-		$woocommerce_plugin_file = dirname( dirname( dirname( __DIR__ ) ) ) . '/woocommerce/woocommerce.php';
-		if ( file_exists( $woocommerce_plugin_file ) ) {
-			require( $woocommerce_plugin_file );
-		} elseif ( file_exists( '/tmp/woocommerce/woocommerce.php' ) ) {
-			require( '/tmp/woocommerce/woocommerce.php' );
-		}
-
-		require( dirname( dirname( __DIR__ ) ) . '/woocommerce-coupon-links.php' );
-	} );
-
-	require $tests_directory . '/includes/bootstrap.php';
-}
-
-function _locate_wordpress_tests_directory() {
-	$directory = getenv( 'WP_TESTS_DIR' );
-
-	if ( ! $directory ) {
-		if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-			$directory = getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit';
-		} elseif ( file_exists( '../../../../../tests/phpunit/includes/bootstrap.php' ) ) {
-			$directory = '../../../../../tests/phpunit';
-		} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
-			$directory = '/tmp/wordpress-tests-lib';
-		}
-	}
-
-	return $directory;
+if ( 'unit' !== $bootstrap->get_test_suite() ) {
+	$directory = $bootstrap->locate_wordpress_tests();
+	$bootstrap->load_tests( $directory );
 }
